7dbbe22390771f447046d3c98983eea8b30a3436
Derek Root <dkwroot@users.noreply.github.com>
2019-11-13 02:58:31 -0600

    Updates

---
+++ precache-manifest.46c6db915bd683ba02213aa379ff51d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af48d594901d0a20e03d71015b9495f",
+    "url": "/3Raw/index.html"
+  },
+  {
+    "revision": "188f26f88c709a94a687",
+    "url": "/3Raw/static/css/2.5c6bdb9e.chunk.css"
+  },
+  {
+    "revision": "f6bf8f01f75fd4779f62",
+    "url": "/3Raw/static/css/main.90566767.chunk.css"
+  },
+  {
+    "revision": "188f26f88c709a94a687",
+    "url": "/3Raw/static/js/2.49014b18.chunk.js"
+  },
+  {
+    "revision": "f6bf8f01f75fd4779f62",
+    "url": "/3Raw/static/js/main.9ff8a5ba.chunk.js"
+  },
+  {
+    "revision": "c3c4078ae603e8bebbe0",
+    "url": "/3Raw/static/js/runtime~main.ebb3bfe1.js"
+  },
+  {
+    "revision": "a93d1ec84013198ec445143d91a8e42b",
+    "url": "/3Raw/static/media/dancer_f_1.a93d1ec8.png"
+  },
+  {
+    "revision": "1a3161027409af2121fdaa5cbdf951dc",
+    "url": "/3Raw/static/media/dancer_f_2.1a316102.png"
+  },
+  {
+    "revision": "50e244614a7fc1ff0d636bc657ecaa76",
+    "url": "/3Raw/static/media/ipadframe1.50e24461.png"
+  },
+  {
+    "revision": "45ba0bb323cf3075a496049f8ac48343",
+    "url": "/3Raw/static/media/logo.45ba0bb3.svg"
+  }
+]);
